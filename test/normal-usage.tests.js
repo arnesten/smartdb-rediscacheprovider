e3e9a54799a1ecc2558c2639a2c4d34dd71e8ec8
Calle Arnesten <calle.arnesten@codekick.com>
2013-04-24 17:32:26 +0200

    Nothing working yet. Just a skeleton.

---
+++ test/normal-usage.tests.js
@@ -0,0 +1,13 @@
+var _ = require('underscore');
+var buster = require('buster');
+var sinon = require('sinon');
+var testCase = buster.testCase;
+var assert = buster.assertions.assert;
+var refute = buster.assertions.refute;
+
+
+module.exports = testCase('normal-usage', {
+	'': function () {
+
+	}
+});
